0c028d615d5ecc5bd896556eaf2af3e6c1c93f2e
andres346274 <37482367+andres346274@users.noreply.github.com>
2018-11-14 13:39:58 +0100

    Update Kalman_GPS.m

--- Kalman_GPS.m
+++ Kalman_GPS.m
@@ -3,14 +3,13 @@
 close all
 clear all
 
-duration = 100; %Duration of the time that the GPS try to estimate the
+duration = 20; %Duration of the time that the GPS try to estimate the
 %position of the vehicle.
-dt = 2;%How often the GPS detects the signal from the correct position of
+dt = .1;%How often the GPS detects the signal from the correct position of
 %the vehicle
 
 
 X_loc = []; %Initialization of the actual position of the vehicle
-v = []; %Initialization of the actual velocity of the vehicle
 X_loc_GPS = []; %Location of the Vehicle according to the GPS
 Y_matrix = zeros(1,duration/dt);%Initialization of the matrix Y
 
@@ -18,18 +17,20 @@ Y_matrix = zeros(1,duration/dt);%Initialization of the matrix Y
 
 A = [1 dt; 0 1]; %State stransition matrix: Prediction of the state of the
 %vehicle
-B = [dt^7; dt];%Input Control matrix: Expected effect of the input
+B = [dt^2/2; dt];%Input Control matrix: Expected effect of the input
 %aceleration in the state of the vehicle
-C = [1 1];% Measurement matrix.
+C = [1 0];% Measurement matrix.
 
-a = 0.1; %Aceleration of the vehicle (let's suppose that is constant)
+a = 0.7; %Aceleration of the vehicle (let's suppose that is constant)
 X = [0; 0]; %Initial state of the vehicle; [posistion; velocity]
 X_estimate = X; %Initailization of the estimation
-Vehicle_noise_magnitude = 0.1; %Noise in the speed of the vehicle
+Vehicle_noise_magnitude = 0.05; %Noise in the speed of the vehicle
 GPS_noise_magnitude = 10; %Noise in the detection of the GPS
-%Eball = Ball_noise_magnitude^2 * [dt^4/4 dt^3/2; dt^3/2 dt^2]; %Covariance in the movement of the vehicle
-%Eblind = Blind_noise_magnitude^2; %Covariance of what the GPS man detects
-%P = Eball; %Estimation of the initial vehicle position variance
+Evehicle = Vehicle_noise_magnitude^2 * [dt^4/4 dt^3/2; dt^3/2 dt^2]; %Covariance in the movement of the vehicle
+EGPS = GPS_noise_magnitude^2; %Covariance of what the GPS man detects
+P = Evehicle; %Estimation of the initial vehicle position variance
+X_loc_estimate = []; %Vehicle position matrix estimated by the Kalman
+
 
 for t = 0: dt : duration
     %The noise change randomly
@@ -44,7 +45,32 @@ for t = 0: dt : duration
     
     plot(0:dt:t, X_loc, '-r.');
     plot(0:dt:t, X_loc_GPS, '-k.');
-    axis([0 100 -1000 1500]);
+    axis([0 20 -30 100]);
     hold on
     
 end
+
+plot(0:dt:t, smooth(X_loc_GPS), '-g.');
+
+X= [0; 0]; % re-initized X
+
+
+for t = 1:length(X_loc)
+   %PREDICTION PART
+    X_estimate = A * X_estimate + B * a;
+    P = A * P * A' + Evehicle; 
+    %UPDATE PART 
+    K = (P*C')/(C*P*C'+EGPS);
+    X_estimate = X_estimate + K * (X_loc_GPS(t) - C * X_estimate);
+    P =(eye(2)-K*C)*P;
+     
+    X_loc_estimate = [X_loc_estimate; X_estimate(1)];
+end
+
+figure(2);
+tt = 0 : dt : duration;
+plot(tt,X_loc,'-r.',tt,X_loc_GPS,'-k.', tt,X_loc_estimate,'-g.');
+axis([0 20 -30 100])
+
+
+
